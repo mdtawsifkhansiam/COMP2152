b6e64056e32a9a54c90965a45486aa299d7ccf73
mdtawsifkhansiam <mdtawsifkhan.siam@georgebrown.ca>
2025-01-11 04:34:31 -0500

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,13 @@
+#Sample Coding Questions 01 Week 01
+#Tawsif, Siam
+
+array = [1, 4, 7, 9]
+
+a,b,c,d = [1,2,3,4]
+e = (a - ((b ** c) // d) + (a % c))
+
+temperature=32.6
+print(f"The temperature today is: {temperature:.3f} degrees Celsius")
+
+userAge=int(input("Enter your age: "))
+print(f"Now showing the shop items filtered by age: {userAge}")
